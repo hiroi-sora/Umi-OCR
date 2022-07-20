10924eb34ec4dfd6b46bc7c7ab8b2e21f9839220
hiroi-sora <2230247019@qq.com>
2022-07-20 20:19:30 +0800

    新功能：任务完成后关机/休眠

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from windnd import hook_dropfiles  # 文件拖拽
 from pyperclip import copy as pyperclipCopy  # 剪贴板
 from webbrowser import open as webOpen  # “关于”面板打开项目网址
 
-ProjectVer = "1.2.4"  # 版本号
+ProjectVer = "1.2.5 Alpha"  # 版本号
 ProjectName = f"Umi-OCR 批量图片转文字 v{ProjectVer}"  # 名称
 ProjectWeb = "https://github.com/hiroi-sora/Umi-OCR"
 TempFilePath = "Umi-OCR_temp"
