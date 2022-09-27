53bba2ef326e7629f7d9073b1e82445591e576f8
hiroi-sora <2230247019@qq.com>
2022-09-27 21:35:47 +0800

    添加横排多行合并后处理

---
+++ ocr/tbpu/__init__.py
@@ -0,0 +1,20 @@
+# tbpu : text block processing unit
+# 文本块处理单元
+
+# OCR返回的结果中，一项包含文字、包围盒、置信度的元素，称为一个“文块” - text block 。
+# 文块不一定是完整的一句话或一个段落。反之，一般是零散的文字。
+# 一个OCR结果常由多个文块组成。
+# 文块处理器就是：将传入的多个文块进行处理，比如合并、排序、删除文块。
+
+from utils.config import Config
+from ocr.tbpu.merge_line_h import TbpuLineH
+from ocr.tbpu.merge_line_h_multi import TbpuLineHMulti
+
+
+Tbpus = {
+    '通用': None,
+    '横排-优化单行': TbpuLineH,
+    '横排-合并多行': TbpuLineHMulti,
+}
+
+Config.set('tbpu', Tbpus)
