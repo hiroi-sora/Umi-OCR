7aa6025c41750ed39fd06ed4e38224f9f7042161
hiroi-sora <2230247019@qq.com>
2022-10-22 12:23:59 +0800

    自然段文块处理：优化逻辑，支持0~2全角空格首行缩进

--- ocr/tbpu/merge_line_h_m_paragraph.py
+++ ocr/tbpu/merge_line_h_m_paragraph.py
@@ -12,10 +12,11 @@ class TbpuLineHMultiParagraph(TbpuLineHMultiLeft):
         # y不接壤，说啥也没用
         if abs(box2[0][1]-box1[3][1]) > self.limitY:
             return False
-        # 若当前是第一行，则额外允许第二行的x位置前移2个全角字符（即行高）
+        # 若当前是第一行，则额外允许第二行的x位置前移最多2个全角字符（即行高）的距离
         if self.mergeNum == 1:
-            x = box2[0][0] + self.rowHeight*2
-            if abs(x-box1[3][0]) <= self.limitX:
-                return True
+            # 第二行x额外允许的范围
+            xMax = box1[3][0] + self.limitX  # 上界：第一行x
+            xMin = box1[3][0] - self.rowHeight*2 - self.limitX  # 下界：第一行x提前2个行高
+            return xMin <= box2[0][0] <= xMax
         # 1的左下角与2的左上角接壤时OK
         return abs(box2[0][0]-box1[3][0]) <= self.limitX
