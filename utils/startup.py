b7a0cc1ddf558f6a7637901c9b357b25763f01df
hiroi-sora <2230247019@qq.com>
2022-10-25 17:22:06 +0800

    增加启动项注册表操作异常处理

--- utils/startup.py
+++ utils/startup.py
@@ -38,23 +38,28 @@ class Startup:
                 Startup.RegRoot, Startup.RegPath, 0, Startup.RegFlag)
         except Exception as e:
             tk.messagebox.showerror(
-                '遇到了一点小问题', '打开注册表失败。请以管理员权限运行软件再重试。')
+                '遇到了一点小问题', '打开注册表失败。请以管理员权限运行软件再重试。\n\n{e}')
             return
-        tips = ''
-        # 移除旧的启动项
-        items = Startup._autoFind(key)
-        if items:
-            for i in items:
-                win32api.RegDeleteValue(key, i)
-            tips += f'移除{len(items)}个旧的开机启动项\n\n'
-        # 添加新的启动项
-        name = Umi.name
-        path = Umi.path
-        win32api.RegSetValueEx(
-            key, name, 0, win32con.REG_SZ, path)
-        win32api.RegCloseKey(key)
-        tips += f'添加开机启动项：【{name}】\n启动路径：{path}'
-        tk.messagebox.showinfo('已设置开机自启', tips)
+        try:
+            tips = ''
+            # 移除旧的启动项
+            items = Startup._autoFind(key)
+            if items:
+                for i in items:
+                    win32api.RegDeleteValue(key, i)
+                tips += f'移除{len(items)}个旧的开机启动项\n\n'
+            # 添加新的启动项
+            name = Umi.name
+            path = Umi.path
+            win32api.RegSetValueEx(
+                key, name, 0, win32con.REG_SZ, path)
+            tips += f'添加开机启动项：【{name}】\n启动路径：{path}'
+            tk.messagebox.showinfo('已设置开机自启', tips)
+        except Exception as e:
+            tk.messagebox.showerror(
+                '遇到了一点小问题', '添加开机启动项失败。请以管理员权限运行软件再重试。\n\n{e}')
+        finally:
+            win32api.RegCloseKey(key)
 
     @staticmethod
     def autoDisable():
@@ -64,16 +69,21 @@ class Startup:
                 Startup.RegRoot, Startup.RegPath, 0, Startup.RegFlag)
         except Exception as e:
             tk.messagebox.showerror(
-                '遇到了一点小问题', '打开注册表失败。请以管理员权限运行软件再重试。')
+                '遇到了一点小问题', '打开注册表失败。请以管理员权限运行软件再重试。\n\n{e}')
             return
-        # 移除旧的启动项
-        items = Startup._autoFind(key)
-        if items:
-            tips = f'移除{len(items)}个旧的开机启动项：'
-            for i in items:
-                win32api.RegDeleteValue(key, i)
-                tips += f'\n{i}'
-        else:
-            tips = '当前没有开机启动项'
-        win32api.RegCloseKey(key)
-        tk.messagebox.showinfo('已禁用开机自启', tips)
+        try:
+            # 移除旧的启动项
+            items = Startup._autoFind(key)
+            if items:
+                tips = f'移除{len(items)}个旧的开机启动项：'
+                for i in items:
+                    win32api.RegDeleteValue(key, i)
+                    tips += f'\n{i}'
+            else:
+                tips = '当前没有开机启动项'
+            tk.messagebox.showinfo('已禁用开机自启', tips)
+        except Exception as e:
+            tk.messagebox.showerror(
+                '遇到了一点小问题', '移除开机启动项失败。请以管理员权限运行软件再重试。\n\n{e}')
+        finally:
+            win32api.RegCloseKey(key)
