0d1d80399819f7d66aa1b94c4ee620beb689cf24
unknown <2230247019@qq.com>
2023-04-22 18:19:47 +0800

    完善：截图子窗口增加锁定功能

--- ui/win_show_image.py
+++ ui/win_show_image.py
@@ -36,6 +36,8 @@ class ShowImage:
         self.win = tk.Toplevel()
         self.win.iconphoto(False, Asset.getImgTK('umiocr24'))  # 设置窗口图标
         self.win.resizable(False, False)  # 禁止原生缩放窗口
+        self.win.title(
+            f'预览 {time.strftime("%H:%M")} （{self.wh[0]}x{self.wh[1]}）')
 
         # 菜单栏
         self.menubar = tk.Menu(self.win)
@@ -45,12 +47,14 @@ class ShowImage:
         self.menubar.add_command(label='保存', command=self.__saveImage)
         # self.menubar.add_command(label='复制', command=self.copyImage)
         submenu = tk.Menu(self.menubar, tearoff=False)
-        submenu.add_command(label='锁定窗口+置顶：Ctrl+T 或 Ctrl+L',
+        submenu.add_command(label='锁定窗口：Ctrl+T 或 Ctrl+L',
                             command=lambda *e: self.__switchLock(1))
         submenu.add_command(label='文字识别：回车', command=self.__ocr)
         submenu.add_command(label='保存图片到本地：Ctrl+S', command=self.__saveImage)
         submenu.add_command(label='复制图片到剪贴板：Ctrl+C', command=self.__copyImage)
         submenu.add_command(label='关闭窗口：Esc', command=self.__onClose)
+        submenu.add_command(label='缩放窗口：左键拖拽右下角箭头图标')
+        submenu.add_command(label='移动窗口：左键拖拽任意位置')
         self.menubar.add_cascade(label='更多', menu=submenu)
 
         # 创建Canvas对象并将其填充为整个窗口
@@ -63,7 +67,7 @@ class ShowImage:
             0, 0, anchor='nw', image=self.imgTK)
 
         # 缩放和移动相关参数
-        imgArr = Asset.getImgTK('zoomArrow48')
+        imgArr = Asset.getImgTK('zoomArrowAlpha48')
         self.zoomSize = (imgArr.width(), imgArr.height())
         self.mouseOriginXY = None  # 本次操作的起始鼠标位置
         self.zoomOriginWH = None  # 本次缩放的起始图片宽高
@@ -75,30 +79,38 @@ class ShowImage:
         self.canvas.itemconfig(self.zoomArrow2, state=tk.HIDDEN)
         self.moveOriginXY = None  # 本次移动的起始窗口位置
 
+        # 锁定相关
+        imgLock = Asset.getImgTK('lockAlpha48')
+        self.isLock = False  # 初始值必为False
+        self.lockX, self.lockY = 0, 0  # 锁定模式下的窗口偏移量
+        self.lockBtn2 = self.canvas.create_image(  # 锁定图标第2层（鼠标进入时显示）
+            0, 0, anchor='nw', image=imgLock)
+        self.lockBtn1 = self.canvas.create_image(  # 锁定图标第1层（鼠标接近时显示）
+            0, 0, anchor='nw', image=imgLock)
+        self.canvas.itemconfig(self.lockBtn1, state=tk.HIDDEN)  # 默认隐藏1、2层
+        self.canvas.itemconfig(self.lockBtn2, state=tk.HIDDEN)
+
         # 绑定事件
         self.win.bind('<Enter>', self.__onWinEnter)  # 鼠标进入窗口
         self.win.bind('<Leave>', self.__onWinLeave)  # 鼠标离开窗口
         self.canvas.bind('<ButtonPress-1>', self.__onCanvasPress)  # 按下画布
         self.canvas.bind('<ButtonRelease-1>', self.__onCanvasRelease)  # 松开画布
         self.canvas.bind('<B1-Motion>', self.__onCanvasMotion)  # 拖拽画布
-        self.canvas.tag_bind(  # 鼠标进入缩放按钮
-            self.zoomArrow1, '<Enter>', self.__onZoomEnter)
-        self.canvas.tag_bind(  # 鼠标离开缩放按钮
-            self.zoomArrow1, '<Leave>', self.__onZoomLeave)
+        # 鼠标进入和离开缩放按钮
+        self.canvas.tag_bind(self.zoomArrow1, '<Enter>', self.__onZoomEnter)
+        self.canvas.tag_bind(self.zoomArrow1, '<Leave>', self.__onZoomLeave)
+        # 鼠标进入、离开锁定按钮
+        self.canvas.tag_bind(self.lockBtn1, '<Enter>', self.__onLockEnter)
+        self.canvas.tag_bind(self.lockBtn1, '<Leave>', self.__onLockLeave)
 
         # 绑定快捷键
         self.win.bind('<Return>', self.__ocr)  # 回车：OCR
-        self.win.bind(  # Ctrl+T：锁定&置顶
-            '<Control-t>', lambda *e: self.__switchLock(0))
-        self.win.bind(  # Ctrl+L：还是锁定&置顶
-            '<Control-l>', lambda *e: self.__switchLock(0))
         self.win.bind('<Control-s>', self.__saveImage)  # Ctrl+S：保存
         self.win.bind('<Control-c>', self.__copyImage)  # Ctrl+C：复制图片
         self.win.bind('<Escape>', self.__onClose)  # Esc：关闭窗口
-
-        # 窗口属性
-        self.__resize(self.imgPIL.width, self.imgPIL.height)  # 设定初始大小
-        self.isLock = False  # 初始值必为False
+        # Ctrl+T 和 ctrl+L：锁定&置顶
+        self.win.bind('<Control-t>', lambda *e: self.__switchLock(0))
+        self.win.bind('<Control-l>', lambda *e: self.__switchLock(0))
 
         # 初始处理
         def start():  # 展开
@@ -107,6 +119,7 @@ class ShowImage:
                 self.win.attributes('-topmost', 0)  # 取消锁定置顶
             self.win.focus()  # 窗口获得焦点
         self.win.after(200, start)
+        self.__resize(self.imgPIL.width, self.imgPIL.height)  # 设定初始大小
 
     def __resize(self, w, h):  # 缩放图片。应用w或h中按图片比例更大的一个值。
         # 适应w或h中比例更大的一个
@@ -143,23 +156,28 @@ class ShowImage:
     # ============================== 事件 ==============================
 
     def __onWinEnter(self, e=None):  # 鼠标进入窗口
-        if self.isLock:
-            return
-        self.canvas.itemconfig(self.zoomArrow1, state=tk.NORMAL)
+        if self.isLock:  # 锁定状态：显示解锁图标
+            self.canvas.itemconfig(self.lockBtn1, state=tk.NORMAL)
+        else:  # 非锁定状态：显示缩放图标
+            self.canvas.itemconfig(self.zoomArrow1, state=tk.NORMAL)
 
     def __onWinLeave(self, e=None):  # 鼠标离开窗口
         self.canvas.itemconfig(self.zoomArrow1, state=tk.HIDDEN)
+        self.canvas.itemconfig(self.lockBtn1, state=tk.HIDDEN)
 
-    def __canvasFunc(self, e, zoomFunc, moveFunc):  # 根据鼠标处于画布哪个位置，执行相应方法
+    def __canvasFunc(self, e, zoomFunc, moveFunc, lockFunc=None):  # 根据鼠标处于画布哪个位置，执行相应方法
         ids = self.canvas.find_withtag(tk.CURRENT)  # 获取当前鼠标位置的元素
-        if self.zoomArrow1 in ids or self.zoomArrow2 in ids:  # 若是缩放按钮
+        if self.zoomArrow1 in ids:  # 若是缩放按钮
             zoomFunc(e)
+        elif self.lockBtn1 in ids and lockFunc:  # 若是锁定按钮
+            lockFunc(e)
         else:  # 若是本体
             moveFunc(e)
 
     def __onCanvasPress(self, e=None):  # 按下画布
         self.__canvasFunc(e, zoomFunc=self.__onZoomPress,
-                          moveFunc=self.__onMovePress)
+                          moveFunc=self.__onMovePress,
+                          lockFunc=self.__onClickUnlock)
 
     def __onCanvasRelease(self, e=None):  # 松开画布
         self.__canvasFunc(e, zoomFunc=self.__onZoomRelease,
@@ -212,6 +230,19 @@ class ShowImage:
         nx, ny = self.moveOriginXY[0]+dx, self.moveOriginXY[1]+dy  # 计算位置设定
         self.win.geometry(f'+{nx}+{ny}')  # 移动窗口
 
+    def __onLockEnter(self, e=None):
+        if not self.isLock:
+            return
+        self.canvas.itemconfig(self.lockBtn2, state=tk.NORMAL)  # 显示2层图标
+        self.canvas.config(cursor='hand2')  # 改变光标为手指
+
+    def __onLockLeave(self, e=None):
+        self.canvas.itemconfig(self.lockBtn2, state=tk.HIDDEN)
+        self.canvas.config(cursor='')  # 改变光标为正常
+
+    def __onClickUnlock(self, e=None):  # 单击解锁
+        self.__switchLock(-1)
+
     # ============================== 功能 ==============================
 
     def __switchLock(self, flag=0):  # 切换：锁定/解锁。
@@ -223,10 +254,25 @@ class ShowImage:
         else:
             self.isLock = False
 
+        winx, winy = self.win.winfo_x(), self.win.winfo_y()  # 当前相对位置
+        self.__onWinLeave()
         if self.isLock:  # 启用锁定
-            self.win.attributes('-topmost', 1)
+            rootx, rooty = self.win.winfo_rootx(), self.win.winfo_rooty()  # 原本的绝对位置
+            self.win.attributes('-topmost', 1)  # 窗口置顶
+            self.win.config(menu='')  # 移除菜单栏
+            self.win.overrideredirect(True)  # 将窗口设置为无边框模式
+            self.win.update()  # 刷新一下
+            # 移动窗口，补偿菜单和边框消失的偏移
+            self.lockX, self.lockY = self.win.winfo_rootx()-rootx, self.win.winfo_rooty()-rooty
+            self.win.geometry(f'+{winx-self.lockX}+{winy-self.lockY}')
         else:  # 解锁
-            self.win.attributes('-topmost', 0)
+            self.canvas.itemconfig(self.zoomArrow1, state=tk.NORMAL)
+            self.win.attributes('-topmost', 0)  # 取消置顶
+            self.win.config(menu=self.menubar)  # 恢复菜单栏
+            self.win.overrideredirect(False)  # 取消无边框模式
+            self.win.update()  # 刷新一下
+            # 移动窗口，补偿菜单和边框恢复的偏移
+            self.win.geometry(f'+{winx+self.lockX}+{winy+self.lockY}')
 
     def __ocr(self, e=None):
         self.__copyImage()
