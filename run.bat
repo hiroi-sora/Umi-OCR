b2c76493f15d766fa7c2f910be4b66941702374b
KmBase <1462011227@qq.com>
2023-07-07 22:01:14 +0800

    增加nuitka打包方式，InnoSetup安装包制作脚本

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM º§ªÓ–Èƒ‚ª∑æ≥
-call E:/Code/MyGit/Umi-OCR//venv//Scripts//activate.bat
-nuitka E:/Code/MyGit/Umi-OCR/main.py --standalone --windows-disable-console --include-data-dir=PaddleOCR-json=PaddleOCR-json --include-data-files=PaddleOCR-json/*dll=PaddleOCR-json/ --plugin-enable=tk-inter --onefile
-pause
--- to_exe.py
+++ to_exe.py
@@ -5,4 +5,4 @@ import os
 dirPath = os.getcwd()
 
 os.system(f'cd /d {dirPath}')
-os.system(r'pyinstaller -F -w -i icon/icon.ico -n "Umi-OCR 文字识别" main.py')
+os.system(r'pyinstaller -F -w -i icon/icon.ico -n "Umi-OCR 文字识别" Umi-OCR.py')
