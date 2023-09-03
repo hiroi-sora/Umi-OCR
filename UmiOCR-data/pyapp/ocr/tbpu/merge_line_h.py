9e8db2e8837103e01dcc6ead08b7717f5f1ee26e
hiroi-sora <2230247019@qq.com>
2023-09-03 22:03:29 +0800

    优化单行合并

--- UmiOCR-data/pyapp/ocr/tbpu/merge_line_h.py
+++ UmiOCR-data/pyapp/ocr/tbpu/merge_line_h.py
@@ -27,33 +27,7 @@ class MergeLineH(Tbpu):
             return True
         return False
 
-    def merge2text(self, text1, text2):  # 合并两段文字
-        # a, b = text1[-1], text2[0]  # 两段的开头/结尾字符
-        # if (
-        #     a.isdigit()  # 数字
-        #     or b.isdigit()  # 字符
-        #     or a.isalpha()
-        #     or b.isalpha()
-        #     or a in Punctuation
-        # ):
-        #     return text1 + " " + text2
-        return text1 + " " + text2
-
-    def sortLines(self, resList):  # 对文块排序，从上到下，从左到右
-        def sortKey(A, B):
-            # 先比较两个文块的水平投影是否重叠
-            ay1, ay2 = A["box"][0][1], A["box"][3][1]
-            by1, by2 = B["box"][0][1], B["box"][3][1]
-            # 不重叠，则按左上角y排序
-            if ay2 < by1 or ay1 > by2:
-                return 0 if ay1 == by1 else (-1 if ay1 < by1 else 1)
-            # 重叠，则按左上角x排序
-            ax, bx = A["box"][0][0], B["box"][0][0]
-            return 0 if ax == bx else (-1 if ax < bx else 1)
-
-        resList.sort(key=cmp_to_key(sortKey))
-
-    def run(self, textBlocks, imgInfo):
+    def mergeLine(self, textBlocks):  # 单行合并
         # 所有文块，按左上角点的x坐标排序
         textBlocks.sort(key=lambda tb: tb["box"][0][0])
         # 遍历每个文块，寻找后续文块中与它接壤、且行高一致的项，合并两个文块
@@ -83,12 +57,31 @@ class MergeLineH(Tbpu):
                     b1[1][0] = b1[2][0] = max(b2[1][0], b2[2][0])  # x右
                     # 合并内容
                     tb1["score"] += tb2["score"]  # 合并置信度
-                    tb1["text"] = self.merge2text(tb1["text"], tb2["text"])  # 合并文本
+                    tb1["text"] = tb1["text"] + " " + tb2["text"]  # 合并文本
                     textBlocks[i] = None  # 置为空，标记删除
             if num > 1:
                 tb1["score"] /= num  # 平均置信度
             resList.append(tb1)  # 装填入结果
+        return resList
+
+    def sortLines(self, resList):  # 对文块排序，从上到下，从左到右
+        def sortKey(A, B):
+            # 先比较两个文块的水平投影是否重叠
+            ay1, ay2 = A["box"][0][1], A["box"][3][1]
+            by1, by2 = B["box"][0][1], B["box"][3][1]
+            # 不重叠，则按左上角y排序
+            if ay2 < by1 or ay1 > by2:
+                return 0 if ay1 == by1 else (-1 if ay1 < by1 else 1)
+            # 重叠，则按左上角x排序
+            ax, bx = A["box"][0][0], B["box"][0][0]
+            return 0 if ax == bx else (-1 if ax < bx else 1)
+
+        resList.sort(key=cmp_to_key(sortKey))
+
+    def run(self, textBlocks, imgInfo):
+        # 单行合并
+        resList = self.mergeLine(textBlocks)
         # 结果排序
         self.sortLines(resList)
-        # 返回新文块组和debug字符串
+        # 返回新文块列表
         return resList
