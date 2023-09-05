548573f30fff1842cdc47bf873209b602e637ed7
hiroi-sora <2230247019@qq.com>
2023-09-05 19:18:50 +0800

    补充获取路径的平台API

--- UmiOCR-data/pyapp/platform/platform_windows.py
+++ UmiOCR-data/pyapp/platform/platform_windows.py
@@ -41,25 +41,18 @@ class _KeyTranslatorApi:
 # ==================== 标准路径 ====================
 # 获取系统的标准路径
 class _StandardPaths:
-    @property
-    def StartMenu(self, type="user"):
+    # 获取开始菜单路径。传值：user 用户菜单 | common 公共菜单
+    @staticmethod
+    def GetStartMenu(type="common"):
         if type == "user":
-            key = winreg.HKEY_CURRENT_USER
-            name = "Start Menu"
+            return os.getenv("APPDATA") + "\\Microsoft\\Windows\\Start Menu"
         elif type == "common":
-            key = winreg.HKEY_LOCAL_MACHINE
-            name = "Common Start Menu"
-        try:
-            reg_key = winreg.OpenKey(
-                key,
-                r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders",
-            )
-            start_menu_path = winreg.QueryValueEx(reg_key, name)[0]
-            winreg.CloseKey(reg_key)
-            return start_menu_path
-        except Exception as e:
-            print("[Error] 无法获取开始菜单路径。", e)
-            return ""
+            return os.getenv("ProgramData") + "\\Microsoft\\Windows\\Start Menu"
+
+    # 获取启动（开机自启）路径。
+    @staticmethod
+    def GetStartup(type="common"):
+        return _StandardPaths.GetStartMenu(type) + "\\Windows\\Start Menu"
 
 
 _KTA = _KeyTranslatorApi()
