a31e943e668142980f078585c289df788c4f6f29
hiroi-sora <2230247019@qq.com>
2023-09-05 12:17:49 +0800

    添加版本文件

---
+++ UmiOCR-data/version.py
@@ -0,0 +1,15 @@
+APP_NAME = "Umi-OCR"
+AUTHOR = "hiroi-sora"
+
+# 主版本号
+MAJOR_VERSION = 2
+# 次版本号
+MINOR_VERSION = 0
+# 修订版本号
+PATCH_VERSION = 0
+# 预发布阶段
+PRE_RELEASE = "dev"
+# 预发布版本号
+PRE_RELEASE_VERSION = 3
+
+# TODO: 补充更多信息
