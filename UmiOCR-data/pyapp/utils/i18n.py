fc58446ae0ea4bfea4e46ec69474419294dd3e17
hiroi-sora <2230247019@qq.com>
2023-09-08 19:41:54 +0800

    优化语言初始化机制

--- UmiOCR-data/pyapp/utils/i18n.py
+++ UmiOCR-data/pyapp/utils/i18n.py
@@ -6,16 +6,37 @@ from PySide2.QtCore import QTranslator
 I18nDir = "i18n"
 I18nConfig = "_"
 I18nConfig = os.path.join(I18nDir, I18nConfig)
+LanguageCodes = {
+    "zh_CN": "简体中文",
+    "zh_TW": "繁體中文",
+    "zh_HK": "繁體中文",
+    "en_US": "English",
+    "en_GB": "English",
+    "en_CA": "English",
+    "es_ES": "Español",
+    "es_MX": "Español",
+    "fr_FR": "Français",
+    "fr_CA": "Français",
+    "de_DE": "Deutsch",
+    "de_AT": "Deutsch",
+    "de_CH": "Deutsch",
+    "ja_JP": "日本語",
+    "ko_KR": "한국어",
+    "ru_RU": "Русский",
+    "pt_BR": "Português",
+    "pt_PT": "Português",
+    "it_IT": "Italiano",
+}
 
 
 class _I18n:
     def init(self, qtApp, trans):
         # 获取信息
-        self.language, self.qmDist = self.__getLangPath()
+        self.__getLangPath()
         if not self.language:
             print("翻译未加载。")
             return
-        path = self.qmDist[self.language]
+        path, text = self.langDict[self.language]
         if not trans.load(path):
             os.MessageBox(
                 f"[Error] Unable to load UI language: {path}\n【异常】无法加载UI语言！",
@@ -28,36 +49,49 @@ class _I18n:
                 info="Umi-OCR Warning",
             )
             return
-        print("翻译加载完毕。")
+        print(f"翻译加载完毕。{self.language} - {text}")
+
+    # 切换语言
+    def changeLanguage(self, lang):
+        if lang in self.langDict:
+            with open(I18nConfig, "w", encoding="utf-8") as file:
+                file.write(lang)
+            return True
+        return False
+
+    # 获取语言字典
+    def getLangDict(self):
+        return self.langDict
 
     # 获取当前翻译文件路径，如果没有配置文件则初始化
     def __getLangPath(self):
-        qmDist = {}
-        lang = ""
+        self.langDict = {}
+        self.language = ""
         for file in os.listdir(I18nDir):
             if file.endswith(".qm"):
-                qmDist[os.path.splitext(file)[0]] = os.path.join(I18nDir, file)
-        print("qm列表：", qmDist)
+                code = os.path.splitext(file)[0]
+                path = os.path.join(I18nDir, file)
+                text = LanguageCodes.get(code, code)
+                self.langDict[code] = (text, path)
+        print("qm列表：", self.langDict)
 
         # 存在配置文件
         if os.path.exists(I18nConfig):
-            with open(I18nConfig, "r") as file:
+            with open(I18nConfig, "r", encoding="utf-8") as file:
                 text = file.read()
-                if text in qmDist:
-                    lang = text
+                if text in self.langDict:
+                    self.language = text
         # 不存在，则初始化配置文件
-        else:
+        if not self.language:
             import locale
 
-            language, encoding = locale.getdefaultlocale()
-            if language in qmDist:
-                lang = language
-                with open(I18nConfig, "w") as file:
-                    file.write(lang)
+            l, encoding = locale.getdefaultlocale()
+            if l in self.langDict:
+                self.language = l
+                with open(I18nConfig, "w", encoding="utf-8") as file:
+                    file.write(l)
             else:
-                print(f"当前系统语言为{language}，无对应翻译文件。")
-
-        return lang, qmDist
+                print(f"当前系统语言为{l}，无对应翻译文件。")
 
 
 I18n = _I18n()
