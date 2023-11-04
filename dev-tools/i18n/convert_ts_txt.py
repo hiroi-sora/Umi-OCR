f31ca8461f961fda4dad0fb519486c7934b4760b
unknown <2230247019@qq.com>
2023-11-04 19:31:53 +0800

    文档

--- dev-tools/i18n/convert_ts_txt.py
+++ dev-tools/i18n/convert_ts_txt.py
@@ -31,10 +31,3 @@ for p in data:
         s += d["source"].replace("\n", r"\n") + "\n"  # 换行转\n
 with open(outFile, "w", encoding="utf-8") as f:
     f.write(s)
-
-"""
-请你充当翻译器，协助我完成一个软件的UI翻译工作。
-我会输入多行文本，每行是一个原文。请你将其翻译为译文。
-请保持原文的特殊格式，原文中的换行、%开头的标识符、mardown等格式予以保留。
-首先，我们来做汉译英。
-"""
