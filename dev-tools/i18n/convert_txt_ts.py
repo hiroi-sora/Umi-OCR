b02a7272d7ada80d1e7a21d941f8d33742f76335
unknown <2230247019@qq.com>
2023-11-04 20:20:41 +0800

    txt转ts

--- dev-tools/i18n/convert_txt_ts.py
+++ dev-tools/i18n/convert_txt_ts.py
@@ -12,24 +12,16 @@ tranIndex = 0
 
 root = tree.getroot()
 
-data = []
 for page in root:
     for message in page:
         if message.tag == "name":
             pass
         else:
-            location = message[0]
-            filename = location.get("filename")
-            source = message[1]
-            translation = message[2]
-            if not source.text:
-                source = message[2]
-                translation = message[3]
-            t = txt[tranIndex].replace(r"\n", "\n")  # \n转换行
-            print("原文：", source.text)
-            print("  译文：", t)
-            tranIndex += 1
-            translation.text = t
-            if translation.attrib.get("type") is not None:
-                translation.attrib.pop("type")
+            for item in message:
+                if item.tag == "translation":
+                    t = txt[tranIndex].replace(r"\n", "\n")  # \n转换行
+                    item.text = t
+                    # if item.attrib.get("type"):
+                    #     item.attrib.pop("type")
+                    tranIndex += 1
 tree.write(outFile)
