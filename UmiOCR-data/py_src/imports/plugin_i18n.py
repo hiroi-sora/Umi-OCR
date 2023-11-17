b589421aee5b0b15da8441de70c0e6417fe332f3
hiroi-sora <2230247019@qq.com>
2023-11-17 08:53:30 +0800

    优化插件翻译机制

--- UmiOCR-data/py_src/imports/plugin_i18n.py
+++ UmiOCR-data/py_src/imports/plugin_i18n.py
@@ -5,7 +5,6 @@
 import os
 import csv
 
-TrDict = {}
 LangCode = ""
 
 
@@ -15,37 +14,36 @@ def setLangCode(langCode):
     LangCode = langCode
 
 
-# 载入翻译。每个模块调用tr前，必须载入翻译。
-# file: 与翻译文件同目录的任意文件的路径
-# name: 翻译文件名
-def trLoad(file="", name=""):
-    global TrDict
-    TrDict = {}
-    if not file or not name:
-        return
-    path = os.path.dirname(os.path.abspath(file)) + "/" + name
-    try:
-        if os.path.exists(path):
-            with open(path, "r", encoding="utf-8") as file:
-                csv_reader = csv.reader(file)
-                headers = next(csv_reader)
-                index = -1
-                for i, h in enumerate(headers):  # 搜索语言
-                    if h == LangCode:
-                        index = i
-                if index == -1:  # 找不到对应语言
-                    if LangCode.startswith("zh_"):  # 中文
-                        index = 0
-                    else:
-                        index = 1  # 找不到对应语言，默认英文
-                for row in csv_reader:
-                    TrDict[row[0]] = row[index]
-    except Exception as e:
-        print("[Error] 加载插件翻译失败：", file, name, e)
+class Translator:
+    # 载入翻译。每个模块调用tr前，必须载入翻译。
+    # file: 与翻译文件同目录的任意文件的路径
+    # name: 翻译文件名
+    def __init__(self, file="", name=""):
+        self._trDict = {}
+        if not file or not name:
+            return
+        path = os.path.dirname(os.path.abspath(file)) + "/" + name
+        try:
+            if os.path.exists(path):
+                with open(path, "r", encoding="utf-8") as file:
+                    csv_reader = csv.reader(file)
+                    headers = next(csv_reader)
+                    index = -1
+                    for i, h in enumerate(headers):  # 搜索语言
+                        if h == LangCode:
+                            index = i
+                    if index == -1:  # 找不到对应语言
+                        if LangCode.startswith("zh_"):  # 中文
+                            index = 0
+                        else:
+                            index = 1  # 找不到对应语言，默认英文
+                    for row in csv_reader:
+                        self._trDict[row[0]] = row[index]
+        except Exception as e:
+            print("[Error] 加载插件翻译失败：", file, name, e)
 
-
-# 进行翻译。输入原文，返回译文
-def tr(original):
-    if original in TrDict:
-        return TrDict[original]
-    return original
+    # 进行翻译。输入原文，返回译文
+    def __call__(self, original):
+        if original in self._trDict:
+            return self._trDict[original]
+        return original
