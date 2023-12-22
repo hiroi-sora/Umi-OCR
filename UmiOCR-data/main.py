19c94d2a4e1abc004f90a82861785c618828ad38
unknown <2230247019@qq.com>
2023-12-22 11:50:52 +0800

    修Bug (#274) 非简体中文系统语言时，类似【'cp950' codec can't encode character '\u8bd1' in position 1】的编码错误。

--- UmiOCR-data/main.py
+++ UmiOCR-data/main.py
@@ -10,15 +10,30 @@ import sys
 import site
 
 
+def MessageBox(msg, type="error"):
+    import ctypes
+
+    info = "Umi-OCR Message"
+    if type == "error":
+        info = "【错误】 Umi-OCR Error"
+    elif type == "warning":
+        info = "【警告】 Umi-OCR Warning"
+    ctypes.windll.user32.MessageBoxW(None, str(msg), str(info), 0)
+    return 0
+
+
+os.MessageBox = MessageBox
+
+
 def initRuntimeEnvironment(startup_script):
     """初始化运行环境"""
 
     # 尝试获取控制台的输出对象
     try:
         fd = os.open("CONOUT$", os.O_RDWR | os.O_BINARY)
-        fp = os.fdopen(fd, "w")
+        fp = os.fdopen(fd, "w", encoding="utf-8")
     except Exception as e:
-        fp = open(os.devnull, "w")
+        fp = open(os.devnull, "w", encoding="utf-8")
     # 输出流不存在时，重定向到控制台
     if not sys.stdout:
         sys.stdout = fp
@@ -27,20 +42,6 @@ def initRuntimeEnvironment(startup_script):
     # def except_hook(cls, exception, traceback):
     #     sys.__excepthook__(cls, exception, traceback)
     # sys.excepthook = except_hook
-
-    # 定义一个最简单的消息弹窗
-    def MessageBox(msg, type="error"):
-        import ctypes
-
-        info = "Umi-OCR Message"
-        if type == "error":
-            info = "【错误】 Umi-OCR Error"
-        elif type == "warning":
-            info = "【警告】 Umi-OCR Warning"
-        ctypes.windll.user32.MessageBoxW(None, str(msg), str(info), 0)
-        return 0
-
-    os.MessageBox = MessageBox
 
     # 初始化工作目录和Python搜索路径
     script = os.path.abspath(startup_script)  # 启动脚本.py的路径
@@ -61,5 +62,12 @@ def runScript():
 
 
 if __name__ == "__main__":
-    initRuntimeEnvironment(__file__)  # 初始化运行环境
-    runScript()  # 启动脚本
+    try:
+        initRuntimeEnvironment(__file__)  # 初始化运行环境
+    except Exception as e:
+        MessageBox("初始化运行环境失败 !\n\n" + str(e))
+        sys.exit(0)
+    try:
+        runScript()  # 启动脚本
+    except Exception as e:
+        MessageBox("主程序启动失败 !\n\n" + str(e))
