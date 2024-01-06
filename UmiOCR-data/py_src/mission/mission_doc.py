cf601de10647c1cef0c876af7fe1abc7ab1d3451
hiroi-sora <2230247019@qq.com>
2024-01-06 12:28:57 +0800

    文档处理 任务流程

--- UmiOCR-data/py_src/mission/mission_doc.py
+++ UmiOCR-data/py_src/mission/mission_doc.py
@@ -24,23 +24,33 @@ class FitzOpen:
 
 
 class _MissionDocClass(Mission):
-    # msnInfo: { 回调函数"onXX", 参数"argd":{"tbpu.xx", "ocr.xx"} }
-    # msnFile: { 可选 "path", "bytes", "base64" }
-    # pageRange 可选：
-    # None - 整本
-    # int - 单页
-    # (1,3) - 页面范围
-    # [1,2,4,5] - 多个页面
-    def addMissionList(self, msnInfo, msnFile, pageRange=None):
-        if "path" in msnFile:
-            doc = fitz.open(msnFile["path"])
-        else:
-            raise Exception("暂不支持 bytes base64 传入PDF")
-        msnInfo["doc"] = doc
-        msnList = list(range(doc.page_count))
-        return super().addMissionList(msnInfo, msnList)
+    def __init__(self):
+        super().__init__()
+        self._schedulingMode = "1234"  # 调度方式：顺序
 
-    def msnTask(self, msnInfo, pno):  # 执行msn
+    # 添加一个文档任务
+    # msnInfo: { 回调函数"onXX", 参数"argd":{"tbpu.xx", "ocr.xx"} }
+    # msnPath: 单个文档路径
+    # pageRange: 页数范围。可选： None 全部页 , [1,3] 页面范围
+    # pageList: 指定多个页数。可选： [] 使用pageRange设置 , [1,2,3] 指定页数
+    def addMission(self, msnInfo, msnPath, pageRange=None, pageList=[]):
+        doc = fitz.open(msnPath)
+        msnInfo["doc"] = doc
+        msnInfo["path"] = msnPath
+        # 使用 pageRange 的页面范围
+        if len(pageList) == 0:
+            if isinstance(pageRange, (tuple, list)) and len(pageRange) == 2:
+                pageList = list(range(pageRange[0], pageRange[1] + 1))
+            else:
+                pageList = list(range(1, doc.page_count + 1))
+        # 检查页数列表合法性
+        if len(pageList) == 0:
+            return "[Error] 页数列表为空"
+        if not all(isinstance(item, int) for item in pageList):
+            return "[Error] 页数列表内容非整数"
+        return self.addMissionList(msnInfo, pageList)
+
+    def msnTask(self, msnInfo, pno):  # 执行msn。pno为当前页数
         doc = msnInfo["doc"]
         page = doc[pno]
         # 获取元素
--- UmiOCR-data/py_src/tag_pages/BatchDOC.py
+++ UmiOCR-data/py_src/tag_pages/BatchDOC.py
@@ -26,5 +26,35 @@ class BatchDOC(Page):
         return docs
 
     def msnDocs(self, docs, argd):
-        print("py文档任务", docs)
+        for d in docs:
+            msnInfo = {
+                "onStart": self._onStart,
+                "onReady": self._onReady,
+                "onGet": self._onGet,
+                "onEnd": self._onEnd,
+                "argd": argd,
+            }
+            path = d["path"]
+            pageRange = [int(d["range_start"]), int(d["range_end"])]
+            msnID = MissionDOC.addMission(msnInfo, path, pageRange)
+            print(f"\n== 任务id {msnID} {path} \n")
+            print(argd)
         return ""
+
+    # ========================= 【任务控制器的异步回调】 =========================
+
+    def _onStart(self, msnInfo):  # 任务队列开始
+        print(f'=== 开始：{msnInfo["path"]}')
+
+    def _onReady(self, msnInfo, page):  # 单个任务准备
+        pass
+        # print(f'准备：{msnInfo["path"][-10:]} {page}')
+
+    def _onGet(self, msnInfo, page, res):  # 单个任务完成
+        pass
+        print(f'获取：{msnInfo["path"][-10:]} {page}')
+
+    def _onEnd(self, msnInfo, page):  # 任务队列完成或失败
+        # msg: [Success] [Warning] [Error]
+        pass
+        print(f'=== 结束：{msnInfo["path"]}')
