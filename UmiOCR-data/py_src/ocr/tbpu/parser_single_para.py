0a954d966b77f15345842c1b90702f5a493b2d78
hiroi-sora <2230247019@qq.com>
2024-02-24 21:52:50 +0800

    tbpu 方案完善： SinglePara

--- UmiOCR-data/py_src/ocr/tbpu/parser_single_para.py
+++ UmiOCR-data/py_src/ocr/tbpu/parser_single_para.py
@@ -1,11 +1,11 @@
 # 排版解析-单栏-自然段
 
-from .tbpu import Tbpu
+from .parser_single_line import SingleLine
 from .parser_tools.line_preprocessing import linePreprocessing  # 行预处理
 from .parser_tools.paragraph_parse import ParagraphParse  # 段内分析器
 
 
-class SinglePara(Tbpu):
+class SinglePara(SingleLine):
     def __init__(self):
         self.tbpuName = "排版解析-单栏-自然段"
 
@@ -13,14 +13,38 @@ class SinglePara(Tbpu):
         get_info = lambda tb: (tb["normalized_bbox"], tb["text"])
 
         def set_end(tb, end):  # 获取预测的块尾分隔符
-            tb["end"] = end
+            tb["line"][-1]["end"] = end
 
         self.pp = ParagraphParse(get_info, set_end)
 
     def run(self, textBlocks):
         textBlocks = linePreprocessing(textBlocks)  # 预处理
-        # 整版分析
-        self.pp.run(textBlocks)  # 预测结尾分隔符
-        for tb in textBlocks:
-            del tb["normalized_bbox"]
+        # 获取每一行
+        lines = self.get_lines(textBlocks)
+        # 将行封装为tb
+        temp_tbs = []
+        for line in lines:
+            b0, b1, b2, b3 = line[0]["normalized_bbox"]
+            # 搜索bbox
+            for i in range(1, len(line)):
+                bb = line[i]["normalized_bbox"]
+                b1 = min(b1, bb[1])
+                b2 = max(b1, bb[2])
+                b3 = max(b1, bb[3])
+            # 构建tb
+            temp_tbs.append(
+                {
+                    "normalized_bbox": (b0, b1, b2, b3),
+                    "text": line[0]["text"][0] + line[-1]["text"][-1],
+                    "line": line,
+                }
+            )
+        # 预测结尾分隔符
+        self.pp.run(temp_tbs)
+        # 解包
+        textBlocks = []
+        for t in temp_tbs:
+            for tb in t["line"]:
+                del tb["normalized_bbox"]
+                textBlocks.append(tb)
         return textBlocks
