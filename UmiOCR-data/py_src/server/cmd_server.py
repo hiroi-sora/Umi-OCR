cb99a0d78acafafa2ba28edc47e309a1ce49f44b
unknown <2230247019@qq.com>
2024-03-18 00:16:03 +0800

    -->和-->> 指令说明与文档添加双引号用法的提示。 (#326)

--- UmiOCR-data/py_src/server/cmd_server.py
+++ UmiOCR-data/py_src/server/cmd_server.py
@@ -291,8 +291,11 @@ class _Cmd:
             help="The function will be called on the child thread and return the result, but it may be unstable or cause QML to crash.",
         )
         # 输出
-        self._parser.add_argument("-->", help="File path for CLI output. (overwrite)")
-        self._parser.add_argument("-->>", help="File path for CLI output. (append)")
+        self._parser.add_argument(
+            "-->",
+            help='The file path for CLI output (overwrite). If the command does not work, please enclose it in double quotes: "-->"',
+        )
+        self._parser.add_argument("-->>", help="The file path for CLI output (append).")
         self._parser.add_argument("paras", nargs="*", help="parameters of [--func].")
 
     # 分析指令，返回指令对象或报错字符串
