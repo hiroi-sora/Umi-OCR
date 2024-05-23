7fb1ee65fb3a824bb4fb95a45407ec8cf6e651f0
hiroi-sora <2230247019@qq.com>
2024-05-23 20:24:23 +0800

    添加 thread_pool 模块

---
+++ UmiOCR-data/py_src/utils/thread_pool.py
@@ -0,0 +1,40 @@
+# =====================================================
+# =============== 全局线程池 异步任务接口 ===============
+# =====================================================
+
+from PySide2.QtCore import QThreadPool, QRunnable
+
+# 全局线程池
+GlobalThreadPool = QThreadPool.globalInstance()
+
+
+# 异步类
+class Runnable(QRunnable):
+    def __init__(self, taskFunc, *args, **kwargs):
+        super().__init__()
+        self._taskFunc = taskFunc
+        self._args = args
+        self._kwargs = kwargs
+
+    def run(self):
+        try:
+            self._taskFunc(*self._args, **self._kwargs)
+        except Exception as e:
+            print(f"[Error] 异步运行发生错误: {e}")
+
+
+# 启动异步类
+def threadPoolStart(runnable: QRunnable):
+    # 检查线程池是否满，并扩充
+    activeThreadCount = GlobalThreadPool.activeThreadCount()
+    if activeThreadCount >= GlobalThreadPool.maxThreadCount():
+        print(f"[Warning] 线程池已满 {activeThreadCount} ！自动扩充+1。")
+        GlobalThreadPool.setMaxThreadCount(activeThreadCount + 1)
+    GlobalThreadPool.start(runnable)
+
+
+# 快捷接口：异步运行函数，返回异步类的对象
+def threadRun(taskFunc, *args, **kwargs):
+    runnable = Runnable(taskFunc, *args, **kwargs)
+    threadPoolStart(runnable)
+    return runnable
