12a7c6aa59447639d866347728dca2c22f5cb72b
hiroi-sora <2230247019@qq.com>
2024-07-05 17:04:36 +0800

    http OCR接口：data.format="text"时，不拼接 末位文本块的间隔符

--- UmiOCR-data/py_src/ocr/output/tools.py
+++ UmiOCR-data/py_src/ocr/output/tools.py
@@ -1,7 +1,9 @@
 # 从data中提取、拼接文本
 def getDataText(data):
     textOut = ""
+    l = len(data) - 1
     for i, tb in enumerate(data):
         textOut += tb["text"]
-        textOut += tb["end"]
+        if i < l:
+            textOut += tb["end"]
     return textOut
